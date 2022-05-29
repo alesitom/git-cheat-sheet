d90d91d796b785f5afed3d74a4c5af67d6f36fb6
alesitom <alemendoza1990@gmail.com>
2022-05-29 01:29:35 -0300

    Add Fira Code font, styling to GitList.jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {gitList} from "./data"
 
 function App() {
   return (
-    <div className="h-screen py-5 font-bold text-center text-teal-200 bg-gray-900">
+    <div className="h-screen py-5 text-center text-teal-200 bg-gray-900">
       <h1 className="font-serif text-3xl">Git Cheat Sheet</h1>
       <GitList list={gitList}/>
     </div>
--- src/components/GitList/GitList.jsx
+++ src/components/GitList/GitList.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const GitList = ({list}) => {
   return (
-    <div>
+    <div className="flex flex-row flex-wrap content-center justify-center">
         {list.map((git, index) => (
-            <div key={index}>
-                <p>{git.category}</p>
+            <div className="flex flex-col items-center justify-center px-4 m-2 rounded-md w-96 h-52 bg-slate-800" key={index}>
+                <p className="font-bold">{git.category}</p>
+                <p className="mt-4 font-firaCode">
+                {git.command}</p>
+                <p className="mt-4 font-sans font-light">
+                    {git.description}
+                </p>
             </div>
         ))}
     </div>
